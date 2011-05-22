b91600402aad133c3d13acf8335f36bce30c7dcd
oppih-374 <letwego28@gmail.com>
2011-05-22 16:47:14 +0800

    write some comments for s003.py for myself to understand

--- p003/s003.py
+++ p003/s003.py
@@ -13,20 +13,25 @@ def func_time(func):
         print func.__name__, "run time:", time.time()-start
     return _wrapper
 
+n = 600851475143
+
 @func_time
 def find_biggest_factor():
-    n = 600851475143
     if n % 2 == 0:
+        """
+        deal with n is even number
+        """
         lastFactor = 2
         n /= 2
         while n % 2 == 0:
             n /= 2
     else:
+        # n is odd number
         lastFactor = 1
 
-    factor = 3
+    factor = 3 # start from a proper number:3
 
-    maxFactor = sqrt(n)
+    maxFactor = sqrt(n) # save time
 
     while n > 1 and factor < maxFactor:
         if n % factor == 0:
@@ -34,13 +39,18 @@ def find_biggest_factor():
             lastFactor = factor
             while n % factor == 0:
                 n /= factor
+                # calculate untill n cannot be dived by lasfFactor
             maxFactor = sqrt(n)
+            # stat a new circle to calculate the factor
         factor += 2
+        # jump to next odd number (it may be a factor)
 
     if n == 1:
         print lastFactor
+        # if n equals lastFator
     else:
         print n
+        # if n cannot be divided anymore
 
 if __name__ == "__main__":
     find_biggest_factor()
