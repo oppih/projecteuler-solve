f5560bd001e279d4739cf4ce5e2ce83f52624633
oppih <letwego28@gmail.com>
2011-05-24 18:59:26 +0800

    work out p005

--- p005/p005.py
+++ p005/p005.py
@@ -9,12 +9,34 @@ def func_time(func):
     def _wrapper(*args, **kwargs):
         start = time.time()
         func(*args, **kwargs)
-        print func.__name__, "run:", time.time()-start
+        print func.__name__, "run time:\t\t", time.time()-start
     return _wrapper
 
 @func_time
+def manually_count():
+    print "manually way:"
+    print 2**4 * 3**2 * 5 * 7 * 11 * 13 * 17 * 19
 
 def gcd(m, n):
-    while not n == 0:
+    while n != 0:
         m, n = n, m % n
-    return m * n / m
+    return m
+
+def gcd_s(a, b):
+    return gcd_s(b, a%b) if b else a # understand? from my test, it speeds up the function by TEN times.
+
+@func_time
+def gcd_reduce():
+    print "use gcd and reduce and lambda:"
+    print reduce(lambda a, b: a*b/gcd(a,b), xrange(1, 20+1))
+
+@func_time
+def gcd_s_reduce():
+    print "use a short gcd with reduce and lambda:"
+    print reduce(lambda a, b: a*b/gcd_s(a,b), xrange(1, 20+1))
+
+if __name__ == "__main__":
+    manually_count()
+    gcd_reduce()
+    gcd_s_reduce()
+
