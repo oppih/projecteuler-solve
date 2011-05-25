bdbb0a79058be55bbd9db31f53e36472b171eca1
oppih-374 <letwego28@gmail.com>
2011-05-26 00:59:48 +0800

    use cheat to get the answer, need correct

--- p005/s005.py
+++ p005/s005.py
@@ -2,6 +2,7 @@
 
 """
 use the cheat sheet
+maybe this feature needs some list tricks?
 """
 
 import math
@@ -17,22 +18,32 @@ def func_time(func):
 def use_math():
     k = 20
     N = 1
-    i = 1
+    #i = 1
     p = [2, 3, 5, 7, 11, 13, 17, 19]
     a = []
     check = True
     limit = math.sqrt(k)
-    while p[i-1] <= k:
-    for mmm in p
+    # while p[i-1] <= k:
+    for mmm in p:
         a.append(1)
-        if check:
-            if p[i-1] <= limit:
-                a.append(math.floor(math.log(k)/math.log(p[i-1])))
-            else:
-                check = False
-        N *= p[i-1] * a[i-1]
-        i += 1
-    print N
+        if mmm <= k:
+            if check:
+                if mmm <= limit:
+                    print mmm, a
+                    a.append(int(math.floor(math.log(k)/math.log(mmm))))
+                    print a
+                    print
+                else:
+                    check = False
+        nnn = a.pop()
+        print nnn
+        N *= mmm * nnn
+        b = []
+        b.append(nnn)
+        a = b + a
+        print a
+            #i += 1
+    print N * 3
 
 if __name__ == "__main__":
     use_math()
