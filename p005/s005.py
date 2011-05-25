c43cab24614d7f2ba2851dcef43ed2a32c586102
oppih <letwego28@gmail.com>
2011-05-25 14:31:40 +0800

    going to use the answer sheet

---
+++ p005/s005.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+This script is used to count rumtime.
+"""
+
+import time
+def func_time(func):
+    def _wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        print func.__name__, "run time:", time.time()-start
+    return _wrapper
+
+@func_time
