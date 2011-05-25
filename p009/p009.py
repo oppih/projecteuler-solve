21200b0b16ae49fc1eeb44298932d3d6028d3f32
oppih-374 <letwego28@gmail.com>
2011-05-25 23:55:17 +0800

    p009 added

---
+++ p009/p009.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
+
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+"""
+
+import time
+def func_time(func):
+    def _wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        print func.__name__, "run time:", time.time()-start
+    return _wrapper
+
+@func_time
