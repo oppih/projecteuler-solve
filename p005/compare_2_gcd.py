6865602a9b2741bf5eb55c50aa2705bc4a854636
oppih <letwego28@gmail.com>
2011-05-25 18:29:40 +0800

    add some comments to the compare script

--- p005/compare_2_gcd.py
+++ p005/compare_2_gcd.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+"""
+this script is to test the speed of the two ways to calculate the gcd fo two random int.
+"""
 import random
 import time
 def func_time(func):
@@ -20,16 +23,19 @@ def gcd_1(a, b):
     return gcd_1(b, a%b) if b else a
     
 @func_time     
-def test_gcd_0(m, n):
-        gcd_0(m, n)
-    
-@func_time
-def test_gcd_1(m, n):
-        gcd_1(m, n)
-        
-if __name__ == "__main__":
+def test_gcd_0():
     for i in xrange (100000):
         m = random.randint(1, 10000)
         n = random.randint(1, 10000)
-        test_gcd_0(m, n)
-        test_gcd_1(m, n)
+        gcd_0(m, n)
+    
+@func_time
+def test_gcd_1():
+    for i in xrange (100000):
+        m = random.randint(1, 10000)
+        n = random.randint(1, 10000)
+        gcd_1(m, n)
+        
+if __name__ == "__main__":
+    test_gcd_0()
+    test_gcd_1()
