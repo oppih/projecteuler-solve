74b529f00b155ae76679dc4f0aade31df037f1fe
oppih-374 <letwego28@gmail.com>
2011-05-25 23:53:05 +0800

    s005 not complete

--- p005/s005.py
+++ p005/s005.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 """
-This script is used to count rumtime.
+use the cheat sheet
 """
 
+import math
 import time
 def func_time(func):
     def _wrapper(*args, **kwargs):
@@ -13,3 +14,25 @@ def func_time(func):
     return _wrapper
 
 @func_time
+def use_math():
+    k = 20
+    N = 1
+    i = 1
+    p = [2, 3, 5, 7, 11, 13, 17, 19]
+    a = []
+    check = True
+    limit = math.sqrt(k)
+    while p[i-1] <= k:
+    for mmm in p
+        a.append(1)
+        if check:
+            if p[i-1] <= limit:
+                a.append(math.floor(math.log(k)/math.log(p[i-1])))
+            else:
+                check = False
+        N *= p[i-1] * a[i-1]
+        i += 1
+    print N
+
+if __name__ == "__main__":
+    use_math()
