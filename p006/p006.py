7f039318d4516656583944e08758ad13db0f26ba
oppih <letwego28@gmail.com>
2011-05-25 18:44:13 +0800

    p006 solved, it's easy

--- p006/p006.py
+++ p006/p006.py
@@ -3,10 +3,10 @@
 """
 The sum of the squares of the first ten natural numbers is,
 
-12 + 22 + ... + 102 = 385
+1^2 + 2^2 + ... + 10^2 = 385
 The square of the sum of the first ten natural numbers is,
 
-(1 + 2 + ... + 10)2 = 552 = 3025
+(1 + 2 + ... + 10)^2 = 55^2 = 3025
 Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
 
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
@@ -21,3 +21,13 @@ def func_time(func):
     return _wrapper
 
 @func_time
+def diffBetSS():
+    m, n, o = 0, 0, 0
+    for i in xrange(1,101):
+        m += i**2
+        n += i
+    o = n**2
+    print o - m
+
+if __name__ == "__main__":
+    diffBetSS()
