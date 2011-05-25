98acd3ffec7453ee6910b3486939c61511bb37ab
oppih-374 <letwego28@gmail.com>
2011-05-26 01:56:08 +0800

    get near to the right answer

--- p005/s005.py
+++ p005/s005.py
@@ -16,34 +16,21 @@ def func_time(func):
 
 @func_time
 def use_math():
-    k = 20
-    N = 1
-    #i = 1
+    k, N = 20, 1
     p = [2, 3, 5, 7, 11, 13, 17, 19]
-    a = []
+    a = [1]*8
     check = True
     limit = math.sqrt(k)
-    # while p[i-1] <= k:
-    for mmm in p:
-        a.append(1)
-        if mmm <= k:
+    for i in xrange(9):
+        if p[i] <= k:
             if check:
-                if mmm <= limit:
-                    print mmm, a
-                    a.append(int(math.floor(math.log(k)/math.log(mmm))))
-                    print a
-                    print
+                if p[i] <= limit:
+                    a[i] = int(math.floor(math.log(k)/math.log(p[i])))
+                    print str(p[i]) + "^" + str(a[i])
                 else:
                     check = False
-        nnn = a.pop()
-        print nnn
-        N *= mmm * nnn
-        b = []
-        b.append(nnn)
-        a = b + a
-        print a
-            #i += 1
-    print N * 3
+        N *= p[i] * a[i]
+        #i += 1
 
 if __name__ == "__main__":
     use_math()
