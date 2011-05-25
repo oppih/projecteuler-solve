f9e3fd5b4cc674f9de441fd1d04b74d53a872ca9
oppih-374 <letwego28@gmail.com>
2011-05-25 23:57:05 +0800

    p010 added

---
+++ p010/p010.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+"""
+
+import time
+def func_time(func):
+    def _wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        print func.__name__, "run time:", time.time()-start
+    return _wrapper
+
+@func_time
