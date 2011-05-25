8c04f2afa2e814a98986eddfc6756cf8535f736f
oppih <letwego28@gmail.com>
2011-05-25 14:34:41 +0800

    starting p006

---
+++ p006/p006.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+The sum of the squares of the first ten natural numbers is,
+
+12 + 22 + ... + 102 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+"""
+
+import time
+def func_time(func):
+    def _wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        print func.__name__, "run time:", time.time()-start
+    return _wrapper
+
+@func_time
