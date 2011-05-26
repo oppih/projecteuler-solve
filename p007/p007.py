9954bbbccecd82c29ccf5257df32d4132f23d932
oppih <letwego28@gmail.com>
2011-05-26 17:40:03 +0800

    work out p007,but it's slow

--- p007/p007.py
+++ p007/p007.py
@@ -14,4 +14,39 @@ def func_time(func):
         print func.__name__, "run time:", time.time()-start
     return _wrapper
 
+# prime numbers are only divisible by unity and themselves
+# (1 is not considered a prime number by convention)
+
+def isprime(n):
+    '''check if integer n is a prime'''
+    # make sure n is a positive integer
+    n = abs(int(n))
+    # 0 and 1 are not primes
+    if n < 2:
+        return False
+    # 2 is the only even prime number
+    if n == 2: 
+        return True    
+    # all other even numbers are not primes
+    # this is cool~ :)
+    if not n & 1: 
+        return False
+    # range starts with 3 and only needs to go up the squareroot of n
+    # for all odd numbers
+    for x in range(3, int(n**0.5)+1, 2):
+        if n % x == 0:
+            return False
+    return True
+
 @func_time
+def find_prime():
+    count = 1
+    prime_s = 3
+    while count < 10001:
+        if isprime(prime_s):
+            count += 1
+        prime_s += 2
+    print prime_s - 2
+
+if __name__ == "__main__":
+    find_prime()
