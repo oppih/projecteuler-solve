0627f46d1ea9099a0c61c666adf51bca4d5957f0
oppih-ub <letwego28@gmail.com>
2011-07-04 18:09:22 +0800

    I want a more autimatically starting a problem

---
+++ start.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+This is written for the automation when starting a new problem.
+"""
+
+problem = sys.argv[1]
+# git checkout -b %s % problem
+# mkdir ./%s % problem
+# cp ./func_time.py ./%s/%s.py % (problem, problem)
+# git add ./%s/%s.py % (problem, problem)
+# git commit -am '%s added' % problem
+# import webbrowser
+# url = http://projecteuler.net/index.php?section=problems&id= + str(int(problem[1:]))
+# webbrowser.open(url)
