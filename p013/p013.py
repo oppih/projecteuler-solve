e3eac251178a8b6387c76009d076c11632de2d42
oppih-ub <letwego28@gmail.com>
2011-07-26 17:50:56 +0800

    result passed :)

--- p013/p013.py
+++ p013/p013.py
@@ -6,10 +6,16 @@ Work out the first ten digits of the sum of the following one-hundred 50-digit n
 
 import time
 def func_time(func):
+    """
+    New decorator from @yongsun
+    """
     def _wrapper(*args, **kwargs):
         start = time.time()
-        func(*args, **kwargs)
+        ret = func(*args, **kwargs)
         print func.__name__, "run time:", time.time()-start
+        return ret
+
+    _wrapper.__name__ = func.__name__
     return _wrapper
 
 def makeList():
