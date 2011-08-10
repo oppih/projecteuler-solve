77157d73b6c7357e97517bde6bea0479b5e59dd8
oppih-ub <letwego28@gmail.com>
2011-08-10 16:57:11 +0800

    find out why, but much slower

--- p030/p030.py
+++ p030/p030.py
@@ -17,9 +17,16 @@ def func_time(func):
     _wrapper.__name__ = func.__name__
     return _wrapper
 
+"""
+The highest 4-digit number is 9999.  9^5 + 9^5 + 9^5 + 9^5 = 236,196
+5 digits, the highest sum of the 5th powers is 295,245
+6 digits, the highest sum of the 5th powers is 354,294
+7 digits, the highest sum of the 5th powers is 413,343
+so, it's 6 digits!
+"""
 @func_time
-def powerDigits(topNum):
-    return sum(x for x in xrange(2, topNum) if sum(int(y)**5 for y in str(x)) == x)
+def powerDigits(powers):
+    return sum(x for x in xrange(2, 9**(powers+1)) if sum(int(y)**5 for y in str(x)) == x)
 
 if __name__ == "__main__":
-    print powerDigits(200000)
+    print powerDigits(5)
