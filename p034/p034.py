b2f38bf72e46b420ab170451f04acc33fbca4473
oppih-ub <letwego28@gmail.com>
2011-08-12 16:14:23 +0800

    use 7*jc(9) for one crazy line, it took 87 seconds...Orz

--- p034/p034.py
+++ p034/p034.py
@@ -27,6 +27,13 @@ zero2nineF = (1,1,2,jc(3),jc(4),jc(5),jc(6),jc(7),jc(8),jc(9))
 def sumFactorionPlus(uplimit):
     return sum(x for x in xrange(3, uplimit) if x == sum(zero2nineF[int(y)] for y in str(x)))
 
+@func_time
+def oneCrazyLine(uplimit):
+    """
+    http://projecteuler.net/index.php?section=forum&id=34
+    """
+    return sum(xxxx for xxxx in [int(reduce(lambda x, y: int(x) + int(y), [str(reduce(lambda x, y: x * y, range(1, int(i) + 1) + [1])) for i in str(j)])) for j in range(0, uplimit) if (j == int(reduce(lambda x, y: int(x) + int(y), [str(reduce(lambda x, y: x * y, range(1, int(i) + 1) + [1])) for i in str(j)])))])
+
 # since:
 # len(str(jc(9)*7)) = 7
 # len(str(jc(9)*8)) = 7
@@ -34,3 +41,4 @@ def sumFactorionPlus(uplimit):
 
 if __name__ == "__main__":
     print sumFactorionPlus(7*jc(9)/60)
+    print oneCrazyLine(7*jc(9))
