38bb129e85d8513fd0fc3c73c45f17dc2bd6947b
oppih-ub <letwego28@gmail.com>
2011-09-19 19:32:45 +0800

    restore to my previous solution

--- p036/p036.py
+++ p036/p036.py
@@ -25,14 +25,11 @@ def func_time(func):
 
 def isPalindrome(n, base):
     digits = []
-    reverses = []
     while n > 0:
         d = str(n % base)
         digits.append(d)
-        reverses.insert(0, d)
         n = n / base
-    #return digits == digits[::-1]
-    return digits == reverses
+    return digits == digits[::-1]
 
 @func_time
 def sumPalindrome():
