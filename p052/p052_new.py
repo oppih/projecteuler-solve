3dd20ad614b3811305c36923e2fcf7b72653db24
oppih-ub <letwego28@gmail.com>
2011-12-31 16:30:23 +0800

    add new way, but do not understand now

---
+++ p052/p052_new.py
@@ -0,0 +1,23 @@
+from func_time import func_time
+"""
+from http://www.s-anand.net/euler.html # 52
+much faster
+"""
+
+def multiples_have_same_digits(n):
+    digit_keys = dict.fromkeys(list(str(n)))
+    for x in xrange(2, 4):
+        for d in list(str(x * n)):
+            if not digit_keys.has_key(d): return False
+    return True
+
+@func_time
+def newSolve():
+    n = 0
+    while True:
+        n = n + 9  # n must be a multiple of 9 for this to happen
+        if multiples_have_same_digits(n):
+            return n
+
+if __name__ == "__main__":
+    print newSolve()
