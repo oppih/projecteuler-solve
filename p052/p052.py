e02641e62b871603f48a767dd32cbb1643a821d7
oppih-ub <letwego28@gmail.com>
2011-12-31 15:43:07 +0800

    first edition, it works. get hint from http://www.itnosh.com/2010/06/Project_Euler_52.html and learn the usage of set(). and from the forum, I get the fact thatL 1/7=0.142857 142857 142857...

--- p052/p052.py
+++ p052/p052.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 
-"This it sample to solve new problems."
+"""
+It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
+
+Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
+"""
 
 from func_time import func_time
 
-def your_solution_function():
-
-
 @func_time
-def yourNewFunctionHere():
-
-
+def findSameDigit():
+    isFound = False
+    while isFound != True:
+        for i in xrange(2,1000000):
+            for j in xrange(2, 7):
+                if set(str(i)) == set(str(i*j)) and j == 6:
+                    isFound = True
+                    break
+                elif set(str(i)) == set(str(i*j)):
+                    continue
+                else:
+                    break
+            if isFound == True:
+                return i
 
 if __name__ == "__main__":
+    print findSameDigit()
 
